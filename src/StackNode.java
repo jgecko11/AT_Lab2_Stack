bee55eb726497d273fd4682bcc49f02c186a8d53
jgecko11 <jgreco22@kentdenver.org>
2020-10-22 13:05:14 -0600

    empty constructor and add E

--- src/StackNode.java
+++ src/StackNode.java
@@ -1,6 +1,16 @@
-public class StackNode {
+/**
+ * Jack Greco
+ * StackNode.java
+ * This is a generic datatype Stack class--it allows the user to only see and interact with the top element
+ * 10-22-20
+ * JMG
+ * @param <E>
+ */
+
+public class StackNode<E>{
+
+    private E e;
 
     StackNode(){}
-
 
 }
